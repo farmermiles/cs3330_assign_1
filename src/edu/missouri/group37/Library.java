7ccd5e6c93f5b5b9d1db01b4fe47de47181ead9b
NoahMalik <nhmkb9@umsystem.edu>
2025-02-12 12:59:08 -0600

    added the ability to remove books, search by ISBN, and display books

--- src/edu/missouri/group37/Library.java
+++ src/edu/missouri/group37/Library.java
@@ -1,42 +1,60 @@
 package edu.missouri.group37;
 
 public class Library {
-<<<<<<< HEAD
 	private Book[] books = new Book[5];
-=======
-    private Book[] books = new Book[5];
->>>>>>> 8677bd7a04667b2e700da63c0557b78b1f84cff1
     private int count = 0;
 
     /***
-     *
+     *Added the ability to store books
      */
     public boolean addBook(Book book) {
     	if (count < books.length) {
     		books[count] = book;
+    		count++;
     		return true;
     	}
         return false;
     }
 
     /***
-     *
+     *Added the ability to remove books
      */
     public boolean removeBook(Book book) {
+    	for (int i=0; i < count; i++) {
+    		if (books[i].equals(book)) {
+    			for (int j = i; j < count -1; j++) {
+    				books[j] = books[j +1];
+    			}
+    			books[count -1] = null;
+    			count--;
+    			return true;
+    		}
+    	}
         return false;
     }
 
     /***
-     *
+     *this searches a book by its ISBN
      */
     public Book searchByISBN(String ISBN) {
+    	for (int i = 0; i < count; i++) {
+    		if (books[i].getISBN().equals(ISBN)) {
+    			return books[i];
+    		}
+    	}
         return null;
     }
 
     /***
-     *
+     *Displays all of the books
      */
     public void displayBooks() {
-        return;
+    	if (count == 0) {
+    		System.out.println("There no books in the library");
+    	}else {
+    		for (int i = 0; i < count; i++) {
+    			System.out.println(books[i].toString());
+    		}
+    	}
     }
 }
